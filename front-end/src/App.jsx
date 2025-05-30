0fd5eadde543edf8c56a6d247cc707bd64ead4ba
Carlacms18 <126569420+Carlacms18@users.noreply.github.com>
2025-05-30 19:49:03 -0100

    feat: criar pagina recuperação de senha

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import RecuperaSenha from "./pages/recuperaSenha";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>IEFP</h1>
+      <RecuperaSenha />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
