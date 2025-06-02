581c46d0f8e2d16e31d6d884a7063e8eaf449d10
Flavio Soares <nerdmaluk@gmail.com>
2025-06-02 19:09:25 -0100

    Configurar sequelize ORM + Postgresql (GA-19)

--- BACKEND/src/db/db.js
+++ BACKEND/src/db/db.js
@@ -1,16 +1,15 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+const sequelize = require("sequelize");
 
-// Caminho absoluto para o arquivo gym.db
-const dbPath = path.resolve(__dirname, 'gym.db');
-
-// Criação/conexão com o banco
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err.message);
-  } else {
-    console.log('Conexão realizada com sucesso ao banco de dados');
-  }
+const connection = new sequelize({
+  dialect: 'sqlite',
+  storage: './gym.sqlite'
 });
+
+const db = {};
+
+db.connection = connection;
+db.sequelize = sequelize;
+db.models = {};
+db.models.User = require("./models/User")(connection, sequelize);
 
 module.exports = db;
