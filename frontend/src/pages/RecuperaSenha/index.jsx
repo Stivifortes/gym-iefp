6cc7f463595981fe1f4cdbf3facbddfd409ba663
Carlacms18 <126569420+Carlacms18@users.noreply.github.com>
2025-06-02 18:50:44 -0100

    Criar pagina recuperacao de senha

---
+++ frontend/src/pages/RecuperaSenha/index.jsx
@@ -0,0 +1,30 @@
+import "./RecuperaSenha.css";
+import Logo from "../../assets/LoginLogo.png";
+
+export default function RecuperaSenha() {
+  return (
+    <div className="recupera-container">
+      <div className="recupera-box">
+        <img alt="Logo" src={Logo} className="logo" />
+        <h2 className="titulo">Redefinição de senha</h2>
+        <h3 className="subtitulo">Preencha os campos abaixo</h3>
+
+        <form className="formulario">
+          <div className="campo">
+            <label htmlFor="email">Digite sua nova senha</label>
+            <input id="email" name="email" type="email" required />
+          </div>
+
+          <div className="campo">
+            <label htmlFor="password">Confira sua nova senha</label>
+            <input id="password" name="password" type="password" required />
+          </div>
+
+          <button type="submit" className="botao">
+            Redefinir senha
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,29 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import Registro from '../pages/Registro';
-import Layout from '../components/Layout';
-import Home from '../pages/Home';
-import Perfil from '../pages/Perfil';
-import Planos from '../pages/Planos';
-import Inscricoes from '../pages/Inscricoes';
-import Utilizadores from '../pages/Utilizadores';
-import Dashboard from '../pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Registro from "../pages/Registro";
+import Layout from "../components/Layout";
+import Home from "../pages/Home";
+import Perfil from "../pages/Perfil";
+import Planos from "../pages/Planos";
+import Inscricoes from "../pages/Inscricoes";
+import Utilizadores from "../pages/Utilizadores";
+import Dashboard from "../pages/Dashboard";
+import RecuperaSenha from "../pages/RecuperaSenha";
 
 export default function AppRoutes() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Layout />} />
-                <Route index element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/registro" element={<Registro />} />
-                <Route path="/perfil" element={<Perfil />} />
-                <Route path="/planos" element={<Planos />} />
-                <Route path="/inscricoes" element={<Inscricoes />} />
-                <Route path="/utilizadores" element={<Utilizadores />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
-
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/planos" element={<Planos />} />
+        <Route path="/inscricoes" element={<Inscricoes />} />
+        <Route path="/utilizadores" element={<Utilizadores />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recupera-senha" element={<RecuperaSenha />} />
+      </Routes>
+    </Router>
+  );
 }
