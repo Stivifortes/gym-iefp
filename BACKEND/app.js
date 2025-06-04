9c796d0fc74564ba312d3efa69847edaab9a87c6
Daniel Pires <danipires9@hotmail.com>
2025-06-04 12:29:50 -0100

    chore: Set up backend project lint and formatting

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const db = require('./src/db/db'); // Caminho corrigido para o db.js
+const express = require('express')
+const db = require('./src/db/db') // Caminho corrigido para o db.js
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World')
+})
 
 // Exemplo opcional para testar a conexão
 
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
+  console.log('Servidor rodando na porta 3000')
+})
 
-module.exports = app;
+module.exports = app
