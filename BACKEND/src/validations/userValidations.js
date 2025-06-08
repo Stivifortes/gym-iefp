3f900ae2a9cd98c58b2b62e568b346fc24daa9dc
Junior Delgado <juprogramador@gmail.com>
2025-06-08 22:42:43 -0100

    change the location of the userValidations file to the Validations folder

---
+++ BACKEND/src/validations/userValidations.js
@@ -0,0 +1,27 @@
+const { body, param } = require('express-validator');
+
+exports.userIdValidation = [
+  param('id')
+    .notEmpty().withMessage('O ID é obrigatório')
+    .isInt().withMessage('O ID deve ser um número inteiro')
+];
+
+exports.updateUserValidation = [
+  body('name')
+    .optional()
+    .trim()
+    .isLength({ min: 2, max: 50 }).withMessage('O nome deve ter entre 2 e 50 caracteres'),
+  
+  body('email')
+    .optional()
+    .trim()
+    .isEmail().withMessage('Forneça um email válido'),
+    
+  body('password')
+    .optional()
+    .isLength({ min: 10 }).withMessage('A senha deve ter pelo menos 10 caracteres'),
+    
+  body('age')
+    .optional()
+    .isInt({ min: 12, max: 100 }).withMessage('A idade deve ser entre 12 e 100 anos')
+];
