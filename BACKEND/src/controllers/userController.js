2745bb7b43df672205a82b35493f1fe83d74b002
juniorgomes14 <118063248+juniorgomes14@users.noreply.github.com>
2025-06-18 09:55:57 -0100

    update

---
+++ BACKEND/src/controllers/userController.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../models');
+const User = db.User;
+
+// SIGN UP
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    // user exist ?
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      return res.status(400).json({ message: 'Usuário já existe.' });
+    }
+
+    // Hash da senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: role || 'user',
+    });
+
+    res.status(201).json({ message: 'Usuário registrado com sucesso!', userId: user.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao registrar usuário.' });
+  }
+};
+
+// LOGIN
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // search user -> email
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    // Comparar a senha
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Senha incorreta.' });
+    }
+
+    // Gerar token JWT
+
+const token = jwt.sign(
+  { id: user.id, email: user.email, role: user.role },
+  process.env.JWT_SECRET,
+  { expiresIn: '2h' }
+);
+
+    res.status(200).json({ message: 'Login realizado com sucesso!', token,user: {
+        name: user.name,
+        email: user.email,
+        role: user.role
+      } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao fazer login.' });
+  }
+};
