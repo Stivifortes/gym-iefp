fc5f5559cfd5cde1914cc8f2d6ce705572e34958
Giovani Santos <jiovanisantos9@gmail.com>
2025-06-20 18:30:32 -0100

    In Progress: Criar rotas para inscricao de users em planos

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const authRoutes = require('./authRoutes')
 const userRoutes = require('./userRoutes')
 const plansRoutes = require('./plans.routes')
+const userPlanRoutes = require('./userPlan.routes')
 
 router.use('/auth', authRoutes)
 router.use('/plans', plansRoutes)
 router.use('/user', userRoutes)
+router.use('/userPlan', userPlanRoutes)
 
 module.exports = router
